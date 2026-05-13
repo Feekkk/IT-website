57c1d9a4dd7ef65ae1dbbbc7429ce5763d016ea7
Feekkk <zxduke350@gmail.com>
2026-05-13 15:30:02 +0800

    update

---
+++ dist/server/assets/_tanstack-start-manifest_v-oN6v5E1_.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/ITD/Documents/IT-website/src/routes/__root.tsx", children: ["/", "/about"], assets: void 0, preloads: ["/assets/index-cKdOf05g.js"] }, "/": { filePath: "C:/Users/ITD/Documents/IT-website/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Cy9H2STT.js"] }, "/about": { filePath: "C:/Users/ITD/Documents/IT-website/src/routes/about.tsx", children: void 0, assets: void 0, preloads: ["/assets/about-BX4mV7tW.js"] } }, clientEntry: "/assets/index-cKdOf05g.js" });
+export {
+  tsrStartManifest
+};
